85bb31a00e0badf4907509bdc634e52436dda21c
BISTU - Syutung <67704893+Syutung2@users.noreply.github.com>
2020-08-03 12:34:37 +0800

    Create scheduleHtmlProvider.js

---
+++ scheduleHtmlProvider.js
@@ -0,0 +1,23 @@
+function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
+//采用小米小爱同学课程表教务导入开发工具默认代码
+                                
+const ifrs = dom.getElementsByTagName("iframe");
+const frs = dom.getElementsByTagName("frame");
+
+if (ifrs.length) {
+    for (let i = 0; i < ifrs.length; i++) {
+        const dom = ifrs[i].contentWindow.document;
+        iframeContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
+        }
+} 
+if (frs.length) {
+    for (let i = 0; i < frs.length; i++) {
+        const dom = frs[i].contentDocument.body.parentElement;
+        frameContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
+        }
+} 
+if(!ifrs.length && !frs.length){
+    return dom.querySelector('body').outerHTML
+}
+return dom.getElementsByTagName('html')[0].innerHTML + iframeContent+frameContent  
+}
